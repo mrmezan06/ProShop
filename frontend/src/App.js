645f67a6813a1d7fbbb052b206ce1d8ff81d39cb
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2023-01-17 00:57:34 +0600

    Implementing React Router and change anchor tag to Link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,23 @@ import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main>
         <Container className="py-3">
-          <HomeScreen />
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
