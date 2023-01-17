cc2402e8b375879e2d649539ebe63cda4420d4e5
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2023-01-17 21:04:53 +0600

    DB connection and colors terminal output created

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const products = require('./data/products');
 const dotenv = require('dotenv');
+const colors = require('colors');
 dotenv.config();
 
 const app = express();
@@ -17,7 +19,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 const PORT = process.env.PORT || 5000;
-app.listen(
-  PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  );
+  connectDB();
+});
