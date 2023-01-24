9fced16b61c06f94be3764ab03e9deacb12ae35e
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2023-01-24 21:42:33 +0600

    Payment screen and save payment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <main>
         <Container className="py-3">
           <Routes>
+            <Route path="/payment" element={<PaymentScreen />} />
             <Route path="/shipping" element={<ShippingScreen />} />
             <Route path="/profile" element={<ProfileScreen />} />
             <Route path="/register" element={<RegisterScreen />} />
