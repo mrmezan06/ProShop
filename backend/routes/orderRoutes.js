55415f5e08260ae9eb2fde7ed95cd77c30a02013
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2023-02-03 20:33:46 +0600

    Update to paid endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 const {
   addOrderItems,
   getOrderById,
+  updateOrderToPaid,
 } = require('../controllers/orderController');
 
 const protect = require('../middleware/authMiddleware');
 
 router.post('/', protect, addOrderItems);
 router.get('/:id', protect, getOrderById);
+router.put('/:id/pay', protect, updateOrderToPaid);
 
 module.exports = router;
