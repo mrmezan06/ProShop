74e1643c722faa95ee788a5971c949da5ae3f904
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2023-02-06 03:10:18 +0600

    Edit Product Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import OrderScreen from './screens/OrderScreen';
 import UserListScreen from './screens/UserListScreen';
 import UserEditScreen from './screens/UserEditScreen';
 import ProductListScreen from './screens/ProductListScreen';
+import ProductEditScreen from './screens/ProductEditScreen';
 
 const App = () => {
   return (
@@ -35,6 +36,10 @@ const App = () => {
             <Route path="/admin/userlist" element={<UserListScreen />} />
             <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
             <Route path="/admin/productlist" element={<ProductListScreen />} />
+            <Route
+              path="/admin/product/:id/edit"
+              element={<ProductEditScreen />}
+            />
             <Route path="/" element={<HomeScreen />} exact />
           </Routes>
         </Container>
