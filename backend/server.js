9b1da6ae64ce60da947ebd60e96455fe07ab1a98
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2023-02-12 17:57:13 +0600

    For Server Setup

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,9 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+// app.get('/', (req, res) => {
+//   res.send('API is running...');
+// });
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
@@ -30,6 +30,17 @@ app.use('/api/upload', uploadRoutes);
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 // __dirname is the current directory
 // const __dirname = path.resolve();
