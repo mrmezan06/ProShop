452ec94f34c61a99874e542fce659326283792c9
Mejanur Rahman Mezan <mrmezan06@gmail.com>
2023-02-13 03:24:53 +0600

    Test for server and client run on same server

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,12 @@ app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
 
-if (process.env.NODE_ENV === 'production') {
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+/* return frontend on PORT 3000 */
+/* if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
@@ -48,14 +53,11 @@ if (process.env.NODE_ENV === 'production') {
   app.get('/', (req, res) => {
     res.send('API is running...');
   });
-}
+} */
 
 // __dirname is the current directory
 // const __dirname = path.resolve();
-app.use(
-  '/uploads',
-  express.static(path.join(__dirname, '/frontend/public/uploads'))
-);
+app.use('/', express.static(path.join(__dirname, '/frontend/public/images')));
 
 // Error handling middleware
 app.use(notFound);
